a6c162527517a27f1c5d919703d1be14cf72673b
刘竞伊 <liujingyi@SakuraMacBook-Pro.local>
2026-01-23 02:16:03 +0900

    fix csrf trusted origins

--- travel_log/settings.py
+++ travel_log/settings.py
@@ -23,13 +23,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-n-hu$9ka*k9@19bi^)wzyo)w(zdwvvxavo=c5k9)0dlk835h!0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "travel-record.onrender.com",
-    "travel-record.onrender.com",
+    ".onrender.com",
     "localhost",
     "127.0.0.1",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://travel-record.onrender.com",
+    "https://*.onrender.com",
 ]
 
 # Application definition
